1a836ceec9f6d104944c09778cc2068e96b1b282
jezzlucena <jezzlucena@gmail.com>
2021-02-17 20:53:23 -0800

    s

--- op_trans/settings.py
+++ op_trans/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = Path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
