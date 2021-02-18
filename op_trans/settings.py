d58534029ecbe0ad9885b7074f83b3ff03dda48f
jezzlucena <jezzlucena@gmail.com>
2021-02-17 20:52:18 -0800

    s

--- op_trans/settings.py
+++ op_trans/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = Path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
