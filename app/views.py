327907ae23cdce622f27748ca3351c88a9598f95
jezzlucena <jezzlucena@gmail.com>
2021-02-18 02:39:19 -0800

    test

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 
 from django.http import Http404
 from django.http import HttpResponse
@@ -39,72 +40,48 @@ def info(request):
 
 
 def _operational_transfrosmation(last_mutation, current_mutation):
-    last_author = last_mutation.author
-    last_origin = last_mutation.origin
     last_data = last_mutation.data
-
-    author = current_mutation.author
-    origin = current_mutation.origin
     data = current_mutation.data
 
-    # Check if all author indices remain the same in the new origin,
-    # except for the last author's index
-    print(origin, last_origin)
-    for key in origin.keys():
-        if key != last_author and origin.get(key) != last_origin.get(key):
-            raise Exception("Error transforming mutation: invalid origin")
+    # Make a copy of all values in the current mutation to avoid changing
+    # important data in the conversation database 
+    new_mutation = Mutation(
+        author=current_mutation.author,
+        conversation=current_mutation.conversation,
+        data=current_mutation.data.copy(),
+        origin=current_mutation.origin.copy())
+
+    new_mutation.origin[last_mutation.author] += 1
+
+    # If last mutation was an insertion and current mutation's index
+    # is greater than or equals last mutation's index, increment
+    # the new mutation's index
+    if last_data["type"] == "insert" and data["index"] >= last_data["index"]:
+        new_mutation.data["index"] += len(last_data["text"])
     
-    # Check if current author's index equals last mutation's author
-    # index incremented by 1, meaning no transformation is necessary
-    if (last_origin[last_author]+1 == origin[last_author]):
-        return current_mutation
+    # Otherwise, if last mutation was a deletion
+    elif last_data["type"] == "delete":
+        last_index = last_data["index"]
+        last_length = last_data["length"]
 
-    # Otherwise, if both mutations have the same origin,
-    # a transformation is necessary
-    elif last_origin == origin:
+        # - If the new mutation is being attempted amidst the deleted characters;
+        #   then set the new index to last mutation's index
+        if data["index"] >= last_index and data["index"] < last_index + last_length:
+            new_mutation.data["index"] = last_index
 
-        # Make a copy of all values in the current mutation to avoid changing
-        # important data in the conversation database 
-        new_mutation = Mutation(
-            author=author,
-            data=data.copy(),
-            origin=origin.copy())
-
-        new_mutation.origin[last_author] += 1
-
-        # If last mutation was an insertion and current mutation's index
-        # is greater than or equals last mutation's index, increment
-        # the new mutation's index
-        if last_data.get("type") == "insert" and data.get("index") >= last_data.get("index"):
-            new_mutation.data["index"] += len(last_data["text"])
-        
-        # Otherwise, if last mutation was a deletion
-        elif last_data.get("type") == "delete":
-            last_index = last_data.get("index")
-            last_length = last_data.get("length")
-
-            # - If the new mutation is being attempted amidst the deleted characters;
-            #   then set the new index to last mutation's index
-            if data.get("index") >= last_index and data.get("index") < last_index + last_length:
-                new_mutation.data["index"] = last_index
-
-            # Otherwise if the new mutation is being attempted at a greater index
-            # than last deletion index+length;
-            # then Subtract last mutation's length from the new index 
-            elif data.get("index") > last_index + last_length:
-                new_mutation.data["index"] -= len(last_data["text"])
-        
-        return new_mutation
-        
-    # Otherwise, a transformation is not possible
-    raise Exception("Error transforming mutation: invalid origin")
+        # Otherwise if the new mutation is being attempted at a greater index
+        # than last deletion index+length;
+        # then Subtract last mutation's length from the new index 
+        elif new_mutation.data["index"] > last_index + last_length:
+            new_mutation.data["index"] -= len(last_data["text"])
+    
+    return new_mutation
 
 
 @csrf_exempt
 def mutations(request):
-    print(request.body)
     if request.method == 'POST':
-        # try:
+        try:
             parsed_data = json.loads(request.body)
             conversation_id = parsed_data.get("conversationId")
             author = parsed_data.get("author")
@@ -112,7 +89,7 @@ def mutations(request):
             origin = parsed_data.get("origin")
 
             conversation, _ = Conversation.objects.get_or_create(identifier=conversation_id)
-            last_mutation = Mutation.objects.filter(conversation__identifier=conversation).last()
+            last_mutation = Mutation.objects.filter(conversation=conversation).last()
             text = conversation.text
             
             data_length = data.get("length", None)
@@ -141,12 +118,31 @@ def mutations(request):
                 "at",
                 data.get("index"))
 
+            is_invalid_mutation = False
             if last_mutation is not None:
-                mutation = _operational_transfrosmation(last_mutation, mutation)
+                last_author = last_mutation.author
+                if last_mutation.origin == mutation.origin and last_author != author:
+                    mutation = _operational_transfrosmation(last_mutation, mutation)
+                else:
+                    last_origin = last_mutation.origin
+
+                    # Check if all author indices remain the same in the new origin,
+                    # except for the last author's index
+                    for key in mutation.origin.keys():
+                        if key != last_author and origin.get(key) != last_origin.get(key):
+                            is_invalid_mutation = True
+                    
+                    # Check if current author's index equals last mutation's author
+                    # index incremented by 1, meaning no transformation is necessary
+                    if last_origin[last_author]+1 != origin[last_author]:
+                        is_invalid_mutation = True
             else:
                 for key in origin.keys():
                     if origin[key] != 0:
-                        raise Exception("Invalid mutation, bad origin for given conversation")
+                        is_invalid_mutation = True
+
+            if is_invalid_mutation:
+                raise Exception("Invalid mutation: wrong origin for this conversation")
 
             index = int(mutation.data["index"])
             if data.get("type") == "insert" and len(text) >= data.get("index"):
@@ -155,15 +151,17 @@ def mutations(request):
                 length = int(mutation.data["length"])
                 conversation.text = text[:index] + text[index+length:]
             else:
-                raise Exception("Invalid mutation type or index")
+                raise Exception("Invalid mutation: missing or invalid type / index / length combination")
 
             mutation.save()
+            conversation.save()
 
             return JsonResponse({
                 "ok": True,
                 "text": conversation.text,
             }, status=201)
-        # except Exception as err:
+        except Exception as err:
+            print(traceback.format_exc())
             return JsonResponse({
                 "msg": err.args[0] if err.args else "Unknown error",
                 "ok": False,
@@ -195,7 +193,8 @@ def conversations(request):
     elif request.method == 'DELETE':
         try:
             Conversation.objects.get(identifier=request.DELETE.get("conversation_id"))
-        except:
+        except Exception as err:
+            print(traceback.format_exc())
             return JsonResponse({
                 "msg": "Conversation not found", 
                 "ok": False,
