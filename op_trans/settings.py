14f9cbe9ef2c0cc7fb773ad41d49f31162a7bfbf
jezzlucena <jezzlucena@gmail.com>
2021-02-18 03:00:12 -0800

    static files

--- op_trans/settings.py
+++ op_trans/settings.py
@@ -41,14 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-print(STATIC_ROOT)
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/staticfiles/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
