bf827f6a5d2b4a628c3ae1df8d0a9f83ddccb4b8
jezzlucena <jezzlucena@gmail.com>
2021-02-20 12:50:27 -0800

    fix static

--- op_trans/settings.py
+++ op_trans/settings.py
@@ -40,10 +40,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(os.path.join(BASE_DIR, 'static')),)
 
-STATIC_ROOT = ''
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    str(os.path.join(BASE_DIR, 'static')),
+)
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
